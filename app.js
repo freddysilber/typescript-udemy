eb040cb636aa7e41bceefeeb41b0b0da882bef5d
freddysilber <freddy.silber@gmail.com>
2020-10-21 21:27:27 -0600

    adds dummy function

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 var logger = 'app.ts is running';
 console.log(logger);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(4, 4.9));
--- app.ts
+++ app.ts
@@ -1,2 +1,7 @@
 const logger: string = 'app.ts is running'
 console.log(logger)
+
+function add(a: number, b: number) {
+	return a + b
+}
+console.log(add(4, 4.9))
