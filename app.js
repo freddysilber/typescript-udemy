b464152020165c330b615958caf5c5a927fafba8
freddysilber <freddy.silber@gmail.com>
2020-10-22 21:23:16 -0600

    adds some code notes

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-var logger = 'app.ts is running';
-console.log(logger);
-function add(a, b) {
-    return a + b;
+var SwearWords;
+(function (SwearWords) {
+    SwearWords[SwearWords["BULLSHIT"] = 0] = "BULLSHIT";
+})(SwearWords || (SwearWords = {})); // typeof: number
+// function to explore adding/ combining things
+function addShit(thingOne, thingTwo) {
+    return +thingOne + +thingTwo;
 }
-console.log(add(4, 4.9));
+// my logs to visualize shit
+console.log(addShit(4, 993));
+console.log(addShit('shit', 'fuck'));
+console.log(addShit(true, 'cunt'));
+console.log(typeof SwearWords.BULLSHIT);
+console.log(SwearWords.BULLSHIT);
+console.log(addShit(SwearWords.BULLSHIT, '69'));
+var mystring = 'we dont need to make this string have a \'string\' type because its already being assined to a string';
+console.log(mystring);
+var myNumber = 21;
+console.log(myNumber);
--- app.ts
+++ app.ts
@@ -1,7 +1,23 @@
-const logger: string = 'app.ts is running'
-console.log(logger)
-
-function add(a: number, b: number) {
-	return a + b
+enum SwearWords { BULLSHIT } // typeof: number
+// custom type here
+type Combinable =
+	| number
+	| string
+	| boolean
+// function to explore adding/ combining things
+function addShit(thingOne: Combinable, thingTwo: Combinable) {
+	return +thingOne + +thingTwo
 }
-console.log(add(4, 4.9))
+// my logs to visualize shit
+console.log(addShit(420, 420))
+console.log(addShit('freddy', 'silber'))
+console.log(addShit(true, 'mrsir'))
+console.log(typeof SwearWords.BULLSHIT)
+console.log(SwearWords.BULLSHIT)
+console.log(addShit(SwearWords.BULLSHIT, '69'))
+
+const mystring = 'we dont need to make this string have a \'string\' type because its already being assigned to a string immediatly and is a const'
+console.log(mystring)
+// Same with this number const, we dont need to set a type because typescript already knows this will be of type number since it is defined and assigned to a number
+const myNumber = 21
+console.log(myNumber)
